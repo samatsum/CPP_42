f67d323a59736b90add3dabfa3e9cbdab0ce39df
Sanshiro Matsumoto <samatsum@c2r3s8.42tokyo.jp>
2025-04-15 15:43:26 +0900

    OKOK

---
+++ ex01/Contact.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <string>
+
+class Contact {
+private:
+	std::string	firstname;
+	std::string	lastname;
+	std::string	nickname;
+	std::string	secret;
+	std::string	number;
+public:
+	const std::string&	get_firstname(void) const;
+	const std::string&	get_lastname(void) const;
+	const std::string&	get_nickname(void) const;
+	const std::string&	get_secret(void) const;
+	const std::string&	get_number(void) const;
+	void				set_firstname(std::string firstname);
+	void				set_lastname(std::string lastname);
+	void				set_nickname(std::string nickname);
+	void				set_secret(std::string secret);
+	void				set_number(std::string number);
+};
